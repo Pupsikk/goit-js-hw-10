8a8c3327aee3e4f3f73ca7aaa97a671456bdb8a3
Alex <100074196+Pupsikk@users.noreply.github.com>
2022-08-09 22:22:42 +0300

    gallery 1 and 2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// получаем доступ к div где будет храниться коллекция
+const galleryContainer = document.querySelector('.gallery');
+// создаем переменую где будет храниться вся галерея
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+// добавляем созданную галерею в разметку
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+            </div>`;
+        })
+.join("");
+}
+
