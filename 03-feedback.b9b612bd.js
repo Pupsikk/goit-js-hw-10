11fa3a8be45c6d322b7249f2bc235f9d71e0a306
Pupsikk <100074196+Pupsikk@users.noreply.github.com>
2022-08-20 17:07:21 +0000

    Deploying to gh-pages from @ Pupsikk/goit-js-hw-08@68648c5b9aa0626a33e0af35d874725a418efeb6 🚀

---
+++ 03-feedback.b9b612bd.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".js-feedback-form"),textarea:document.querySelector(".js-feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefauld()})),e.textarea.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-msg",t)}))}();
+//# sourceMappingURL=03-feedback.b9b612bd.js.map
---
+++ 03-feedback.d2df8904.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".js-feedback-form"),textarea:document.querySelector(".js-feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefauld()})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-msg",t)}));
+//# sourceMappingURL=03-feedback.d2df8904.js.map
