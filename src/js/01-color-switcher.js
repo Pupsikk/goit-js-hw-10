28b637773e74db1bc0a319f18757519e878e8681
Alex <100074196+Pupsikk@users.noreply.github.com>
2022-08-26 14:27:10 +0300

    task 1 finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,53 @@
+// Импортируем 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+// Создание рандомного цвета
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+//Создаем в html коде стиль backgroundColor
+function updateBodyBGcolor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+class ColorSwitcher {
+    constructor(updateBodyBGcolor) {
+    this.intervalID = null;
+    this.isActive = false;
+    this.updateBodyBGcolor = updateBodyBGcolor;
+    stopBtn.disabled = true;
+    }
+
+    //Старт для начала рандома цветов и делает кнопку не активной  
+    startChangeBGcolor() {
+        if (this.isActive) {
+        return;
+        }
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+
+        this.isActive = true;
+        this.intervalID = setInterval(
+            () => updateBodyBGcolor(getRandomHexColor()),
+            1000
+        );
+    }
+    
+    //Останавливает рандом цветов и делает кнопку не активной  
+    stopChangeBGcolor() {
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+
+        clearInterval(this.intervalID);
+        this.isActive = false;
+    }
+}
+
+const colorSwitcher = new ColorSwitcher();
+
+startBtn.addEventListener('click', () => colorSwitcher.startChangeBGcolor());
+stopBtn.addEventListener('click', () => colorSwitcher.stopChangeBGcolor());
